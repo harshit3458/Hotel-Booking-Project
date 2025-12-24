e2fb26037f7e4a3a4d0b405c09ebbb7f7f10862d
harshit3458 <harshit3458@gmail.com>
2025-12-24 11:28:42 +0530

    Project completed

--- client/src/Pages/hotelOwner/Dashboard.jsx
+++ client/src/Pages/hotelOwner/Dashboard.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Title from '../../Components/Title'
 import { assets, dashboardDummyData } from '../../assets/assets'
 import { useState } from 'react'
+import {useAppContext} from '../../context/AppContext'
 
 const Dashboard = () => {
-   const [dashboardData,setDashboardData]=useState(dashboardDummyData)
+   const [dashboardData,setDashboardData]=useState({
+    bookings:[],
+    totalBookings:0,
+    totalRevenue:0,
+   })
+
+   const {currency,user,getToken,toast,axios}=useAppContext()
+
+   const fetchDashboardData=async ()=>{
+    try {
+      const {data}= await axios.get('/api/bookings/hotel',{headers:{Authorization:`Bearer ${await getToken()}`}})
+      if(data.success){
+        setDashboardData(data.dashboardData)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+   }
+
+   useEffect(()=>{
+      if(user){
+        fetchDashboardData();
+      }
+   },[user])
+
+
   return (
     <div>
       <Title align="left" font="outfit" title="Dashboard" subTitle="Monitor your room listings, track bookings and analyze revenue-all in one place. Stay updated with real time insights to ensure smooth operations." />
